06a4427e3677b19c436dda07d76ccf76467a1663
Ardit <ardit.hoxha@gmail.com>
2025-02-11 15:33:48 -0500

    Initial commit

---
+++ src/main/java/com/ardithoxha/jobAppTracker/JobAppTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.ardithoxha.jobAppTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAppTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobAppTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ardithoxha/jobAppTracker/JobAppTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ardithoxha.jobAppTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAppTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
